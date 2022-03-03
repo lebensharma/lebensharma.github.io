12990d593b3c58d97b84cdece601e470b2f27ad6
lebensharma <aashu.0704@zohomail.in>
2022-03-04 03:00:18 +0530

    updated home page for github

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
-$(document).ready(function(){
-    setTimeout(function(){
-        $('body').addClass('loaded');
-    }, 3000);
-});
+function stopRotating() {
+    document.getElementById("swingwheel").style.animationPlayState="paused";
+    document.getElementById("seatwheel").style.animationPlayState="paused";
+    document.getElementById("seat-1").style.animationPlayState="paused";
+    document.getElementById("seat-2").style.animationPlayState="paused";
+    document.getElementById("seat-3").style.animationPlayState="paused";
+    document.getElementById("seat-4").style.animationPlayState="paused";
+    document.getElementById("seat-5").style.animationPlayState="paused";
+    document.getElementById("seat-6").style.animationPlayState="paused";
+    document.getElementById("seat-7").style.animationPlayState="paused";
+    document.getElementById("seat-8").style.animationPlayState="paused";
+};
 
+function startRotating() {
+    document.getElementById("swingwheel").style.animationPlayState="running";
+    document.getElementById("seatwheel").style.animationPlayState="running";
+    document.getElementById("seat-1").style.animationPlayState="running";
+    document.getElementById("seat-2").style.animationPlayState="running";
+    document.getElementById("seat-3").style.animationPlayState="running";
+    document.getElementById("seat-4").style.animationPlayState="running";
+    document.getElementById("seat-5").style.animationPlayState="running";
+    document.getElementById("seat-6").style.animationPlayState="running";
+    document.getElementById("seat-7").style.animationPlayState="running";
+    document.getElementById("seat-8").style.animationPlayState="running";
+};
 
-setInterval(() => {
-    var one_day = 1000 * 60 * 60 * 24;
-    var date_target = new Date(2021, 3, 28);
-    var date_today = new Date();
-    resultd = Math.round((date_target.getTime() - date_today.getTime()) / (one_day));
-    h = resultd * 24 + (23 - date_today.getHours())
-    m = (59 - date_today.getMinutes())
-    s = (60 - date_today.getSeconds())
-    document.getElementById("d").innerHTML = resultd + "d";
-    document.title = "Live after " + h + "h " + m + "m " + s + "s"
-}, 1000);
-
-// if resultd > 30
-
-
-
-setInterval(() => {
-    d = new Date();
-    h = 23 - (new Date().getHours());
-    m = 59 - (new Date().getMinutes());
-    s = 60 - (new Date().getSeconds());
-
-    hrotation = 30 * h + m / 2;
-    mrotation = 6 * m;
-    srotation = 6 * s;
-
-    hour.style.transform = `rotate(${hrotation}deg)`;
-    minute.style.transform = `rotate(${mrotation}deg)`;
-    second.style.transform = `rotate(${srotation}deg)`;
-
-}, 1000);
